a1add0ea12c420af80605dcccc1a6451337d1965
WIN-OPM3TB4AK0M\ICT02_19 <asdf@gmail.com>
2024-10-02 11:40:02 +0900

    부트스트랩 적용 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react'
 
-
 function App() {
-  
-  const hello = "안녕하세요";
+    const hello = '안녕하세요'
 
-  const style = {
-    fontSize : 30
-  }
+    const style = {
+        fontSize: 30,
+    }
 
-  return (
-    <>
-      <div style={style}>{hello}</div>
-      <div>{hello}</div>
-    </>
-  )
+    return (
+        <>
+            <div style={style}>{hello}</div>
+            <div>{hello}</div>
+        </>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
-
+import BootStrapButton from './BootStrapButton.jsx'
+import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Header />
+        <App />
+        <BootStrapButton />
+    </StrictMode>
 )
