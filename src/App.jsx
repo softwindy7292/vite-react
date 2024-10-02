9d3e540b8f98f79a9448ea2af286634a96e2579b
WIN-OPM3TB4AK0M\ICT02_19 <asdf@gmail.com>
2024-10-02 14:34:00 +0900

    라우터 링크 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import BoardPage from './pages/BoardPage'
-import ProfilePage from './pages/ProfilePage'
+import routes from './route/Routes'
+import HeaderNav from './components/HeaderNav'
 
 function App() {
     return (
         <>
             <BrowserRouter>
+                <HeaderNav />
                 <Routes>
-                    <Route path="/" element={<HomePage />}></Route>
-                    <Route path="/board" element={<BoardPage />}></Route>
-                    <Route path="/profile" element={<ProfilePage />}></Route>
+                    {routes.map((route, index) => {
+                        return (
+                            <Route
+                                key={route.path || index}
+                                path={route.path}
+                                element={route.element}
+                            ></Route>
+                        )
+                    })}
                 </Routes>
             </BrowserRouter>
         </>
